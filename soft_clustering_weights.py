c3fa76f022cf39061970ff23adf58687b016c3fd
mattcrooksphd <52741052+mattcrooksphd@users.noreply.github.com>
2019-07-11 16:43:32 +0100

    added soft_cluster_weights function

---
+++ soft_clustering_weights.py
@@ -0,0 +1,19 @@
+def soft_clustering_weights(data, cluster_centres):
+    
+    Nclusters = cluster_centres.shape[0]
+    Ndp = data.shape[0]
+    Nfeatures = data.shape[1]
+
+    # Get distances from the cluster centres for each data point and each cluster
+    EuclidDist = np.zeros((Ndp, Nclusters))
+    for i in range(Nclusters):
+        EuclidDist[:,i] = np.sum((data-np.matlib.repmat(cluster_centres[i], Ndp, 1))**2,axis=1)
+    
+    # Fuzziness parameter m>=1. Where m=1 => hard segmentation
+    m = 2
+    
+    # Denominator of the weight from wikipedia:
+    invWeight = EuclidDist**(2/(m-1))*np.matlib.repmat(np.sum((1./EuclidDist)**(2/(m-1)),axis=1).reshape(-1,1),1,Nclusters)
+    Weight = 1./invWeight
+    
+    return Weight
