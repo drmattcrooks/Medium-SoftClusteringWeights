4a41fd5c556e6b788a734d4ee3646d9bd7465894
mattcrooksphd <52741052+mattcrooksphd@users.noreply.github.com>
2019-07-11 16:47:29 +0100

    commented soft_clustering_weights function

--- soft_clustering_weights.py
+++ soft_clustering_weights.py
@@ -1,4 +1,16 @@
-def soft_clustering_weights(data, cluster_centres):
+def soft_clustering_weights(data, cluster_centres, **kwargs):
+    
+    """
+    Function to calculate the weights from soft k-means
+    data: Array of data. Features arranged across the columns with each row being a different data point
+    cluster_centres: array of cluster centres. Input kmeans.cluster_centres_ directly.
+    param: m - keyword argument, fuzziness of the clustering. Default 2
+    """
+    
+    # Fuzziness parameter m>=1. Where m=1 => hard segmentation
+    m = 2
+    if 'm' in kwargs:
+        m = kwargs['m']
     
     Nclusters = cluster_centres.shape[0]
     Ndp = data.shape[0]
@@ -9,8 +21,7 @@ def soft_clustering_weights(data, cluster_centres):
     for i in range(Nclusters):
         EuclidDist[:,i] = np.sum((data-np.matlib.repmat(cluster_centres[i], Ndp, 1))**2,axis=1)
     
-    # Fuzziness parameter m>=1. Where m=1 => hard segmentation
-    m = 2
+
     
     # Denominator of the weight from wikipedia:
     invWeight = EuclidDist**(2/(m-1))*np.matlib.repmat(np.sum((1./EuclidDist)**(2/(m-1)),axis=1).reshape(-1,1),1,Nclusters)
